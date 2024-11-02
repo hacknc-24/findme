a7c9d69ac0407c5e876c3b276a968faa7e6b01da
Varun G <varungadde@outlook.com>
2024-11-02 18:41:56 -0400

    Fix no home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "./Pages/Auth/Auth";
 import Profile from "./Pages/Profile/Profile.tsx";
-
+import Dashboard from "./Pages/Dashboard/Dashboard.tsx";
 function App() {
   return (
     <>
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/profile" element={<Profile />}/>
           <Route strict path="/" element={<Auth />} />
+          <Route strict path="/home" element={<Dashboard />} />
         </Routes>
       </Router>
     </>
