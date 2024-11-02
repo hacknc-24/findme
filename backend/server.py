f568033bc89f5dcb7e1408cee241d7f2a24a377f
Varun G <varungadde@outlook.com>
2024-11-02 12:48:17 -0400

    m

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://findme:findme@localhost:27017/findme")
 app = Flask(__name__)
+db = client['your-database']
 
 @app.route("/")
 def hello_world():
