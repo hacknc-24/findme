7d3663509abc62342c93e164947c2b1863213402
@stillascold <stillascold@gmail.com>
2024-11-02 14:49:34 -0400

    Semi-Setup backend with Nest

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/', {
-      user: 'findme',
-      pass: 'findme',
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
