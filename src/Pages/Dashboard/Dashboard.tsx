d01f22fa636b2997f0a7d28fcd9d494f6cff61b9
@stillascold <stillascold@gmail.com>
2024-11-02 23:52:57 -0400

    deploy fix

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -2,19 +2,19 @@ import Navbar from "../../Components/Navbar/Navbar"
 import './dashboardStyle.css'
 
 
-const Dashboard = () => {
-  const mockUsers = [
-    { id: 1, name: 'User 1', description: 'Description for User 1' },
-    { id: 2, name: 'User 2', description: 'Description for User 2' },
-    { id: 3, name: 'User 3', description: 'Description for User 3' },
-    { id: 4, name: 'User 4', description: 'Description for User 4' },
-    { id: 5, name: 'User 5', description: 'Description for User 5' },
-    { id: 6, name: 'User 6', description: 'Description for User 6' },
-    { id: 7, name: 'User 7', description: 'Description for User 7' },
-    { id: 8, name: 'User 8', description: 'Description for User 8' },
-    { id: 9, name: 'User 9', description: 'Description for User 9' },
-    { id: 10, name: 'User 10', description: 'Description for User 10' },
-  ];
+// const Dashboard = () => {
+//   const mockUsers = [
+//     { id: 1, name: 'User 1', description: 'Description for User 1' },
+//     { id: 2, name: 'User 2', description: 'Description for User 2' },
+//     { id: 3, name: 'User 3', description: 'Description for User 3' },
+//     { id: 4, name: 'User 4', description: 'Description for User 4' },
+//     { id: 5, name: 'User 5', description: 'Description for User 5' },
+//     { id: 6, name: 'User 6', description: 'Description for User 6' },
+//     { id: 7, name: 'User 7', description: 'Description for User 7' },
+//     { id: 8, name: 'User 8', description: 'Description for User 8' },
+//     { id: 9, name: 'User 9', description: 'Description for User 9' },
+//     { id: 10, name: 'User 10', description: 'Description for User 10' },
+//   ];
 
   // const sendLocation = (data: Object) => {
   //   fetch("", {
@@ -26,21 +26,21 @@ const Dashboard = () => {
   //     })
   //   })
   // }
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log("Latitude:", position.coords.latitude);
-          console.log("Longitude:", position.coords.longitude);
-          const return_data = {
-            "latitude": position.coords.latitude,
-            "longitude": position.coords.longitude
-          }
-          return return_data
-        }
-      )
-    }
-  }
+  // const getLocation = () => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         console.log("Latitude:", position.coords.latitude);
+  //         console.log("Longitude:", position.coords.longitude);
+  //         const return_data = {
+  //           "latitude": position.coords.latitude,
+  //           "longitude": position.coords.longitude
+  //         }
+  //         return return_data
+  //       }
+  //     )
+  //   }
+  // }
   return (
     <>
       <Navbar />
